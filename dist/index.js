ca48e0568349d9ebcec06e7969c9ca463e398446
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-08-07 09:49:38 -0600

    feat: initialize backend with TypeScript, Express, and necessary configurations

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/ping', (req, res) => {
+    return res.json({
+        message: 'pong'
+    });
+});
+app.listen(3000, () => {
+    console.log("Server is running on the port 3000");
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express,{ Express,Request,  Response} from 'express'
+
+const app: Express = express();
+
+app.get('/ping',(_ : Request,res : Response)=>{
+        return res.json({
+            message:'pong'
+        });
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on the port 3000");
+    
+});
